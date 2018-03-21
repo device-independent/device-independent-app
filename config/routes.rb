6aaac4c34a694fecfcedcdbb5649b303614178d6
Nate Klaiber <nate@theklaibers.com>
2018-03-21 16:15:11 -0400

    Add separate pages to test functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   namespace(:extole) do
     root to: 'pages#home'
+
+    get 'product', to: 'pages#product'
+    get 'registration', to: 'pages#registration'
+    get 'order/confirmation', to: 'pages#confirmation'
+    get 'share', to: 'pages#share'
+    get 'conversion', to: 'pages#conversion'
   end
 
 end
